53e0f4ea265bb41974ccc3cbf189cb9986941ecf
Ankesh Mishra <69436610+GitAnkesh@users.noreply.github.com>
2024-10-25 20:18:39 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (10.40%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.java
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.java
@@ -1,22 +1,41 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        if(nums.length==0){
-            return 0;
-        }
-        int i = 0; // Pointer for placing the next unique element
-        int count = 1; // Counter for occurrences of each element
+        // if(nums.length==0){
+        //     return 0;
+        // }
+        // int i = 0; // Pointer for placing the next unique element
+        // int count = 1; // Counter for occurrences of each element
 
-        for (int j = 1; j < nums.length; j++) {
-            if (nums[j] == nums[i]) {
-                if (count < 2) {
-                    nums[++i] = nums[j];
+        // for (int j = 1; j < nums.length; j++) {
+        //     if (nums[j] == nums[i]) {
+        //         if (count < 2) {
+        //             nums[++i] = nums[j];
+        //         }
+        //         count++;
+        //     } else {
+        //         nums[++i] = nums[j];
+        //         count = 1; // Reset count for the new element
+        //     }
+        // }
+        // return i + 1;
+        if(nums.length==0) return 0;
+
+        int count =1;
+        int i=0;
+        for (int j=1;j<nums.length;j++){
+            if(nums[j]==nums[i]){
+                if(count<2){
+                nums[++i]=nums[j];
                 }
-                count++;
-            } else {
-                nums[++i] = nums[j];
-                count = 1; // Reset count for the new element
+
+
+                count ++;
+            }
+            else{
+                nums[++i]=nums[j];
+                count =1;
             }
         }
-        return i + 1;
+        return i+1;
     }
 }
